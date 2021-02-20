36ba96a718c7dfba1bb1e02983192a89b290decd
yongsik <mymyyoo@naver.com>
2021-02-20 14:07:12 +0900

    Add codility code - Lesson02. Cyclic Rotation

---
+++ src/main/java/gracefulsoul/codility/lesson01/CyclicRotation.java
@@ -0,0 +1,27 @@
+package gracefulsoul.codility.lesson01;
+
+import java.util.Arrays;
+
+public class CyclicRotation {
+
+	public static void main(String[] args) {
+		print(solution(new int[] {3,8,9,7,6}, 3));
+		print(solution(new int[] {1,2,3,4}, 4));
+	}
+	
+	private static int[] solution(int[] A, int K) {
+        int length = A.length;
+        int[] result = new int[length];
+        // Loop A's length.
+        for (int idx = 0; idx < length; idx++) {
+            // Move A[idx] value to result array.
+            result[(length + idx + K) % length] = A[idx];
+        }
+        return result;
+	}
+	
+	private static void print(int[] array) {
+		System.out.println(Arrays.toString(array));
+	}
+
+}
