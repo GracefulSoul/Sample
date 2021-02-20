fd462f5ea7eef2e2b63ea64b79d9beeb975a320e
yongsik <mymyyoo@naver.com>
2021-02-20 15:05:15 +0900

    Add codility code - Lesson03. PermMissingElem

--- src/main/java/gracefulsoul/codility/lesson03/FrogJmp.java
+++ src/main/java/gracefulsoul/codility/lesson03/FrogJmp.java
@@ -6,6 +6,7 @@ public class FrogJmp {
 		System.out.println(solution(10, 85, 30));
 	}
 	
+	// https://app.codility.com/demo/results/training8JZJC2-DF5/
 	private static int solution(int X, int Y, int D) {
 		return (int) Math.ceil((double)(Y - X) / D);
 	}
