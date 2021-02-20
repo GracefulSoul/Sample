0416f0e8700272712222dbbe25b316d631da84ff
yongsik <mymyyoo@naver.com>
2021-02-20 15:36:51 +0900

    Add codility code - Lesson04. FrogRiverOne

--- src/main/java/gracefulsoul/codility/lesson04/FrogRiverOne.java
+++ src/main/java/gracefulsoul/codility/lesson04/FrogRiverOne.java
@@ -9,18 +9,18 @@ public class FrogRiverOne {
 		System.out.println(solution(5, new int[] {1,3,1,4,2,3,5,4}));
 		System.out.println(solution(2, new int[] {2,2,2,2,2}));
 	}
-	
+
 	// https://app.codility.com/demo/results/trainingJ8UD3S-3CE/
-    private static int solution(int X, int[] A) {
-        Set<Integer> leafs = new HashSet<>();
-        for (int idx = 0; idx < A.length; idx++) {
-            leafs.add(A[idx]);
-            // Get to the goal.
-            if (X == leafs.size()) {
-                return idx;
-            }
-        }
-        return -1;
-    }
+	private static int solution(int X, int[] A) {
+		Set<Integer> leafs = new HashSet<>();
+		for (int idx = 0; idx < A.length; idx++) {
+			leafs.add(A[idx]);
+			// Get to the goal.
+			if (X == leafs.size()) {
+				return idx;
+			}
+		}
+		return -1;
+	}
 
 }
