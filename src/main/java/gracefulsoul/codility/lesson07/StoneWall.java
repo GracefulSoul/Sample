b81428b8c6248f14501afe1c17a88b346712ffde
yongsik <mymyyoo@naver.com>
2021-02-21 14:29:23 +0900

    Add codility code - Lesson07. StoneWall

---
+++ src/main/java/gracefulsoul/codility/lesson07/StoneWall.java
@@ -0,0 +1,27 @@
+package gracefulsoul.codility.lesson07;
+
+import java.util.Stack;
+
+public class StoneWall {
+
+	public static void main(String[] args) {
+		System.out.println(solution(new int[] {8,8,5,7,9,8,7,4,8}));
+	}
+
+	// https://app.codility.com/demo/results/training6V5C8T-EW8/
+	public static int solution(int[] H) {
+		Stack<Integer> stack = new Stack<>();
+		int count = 0;
+		for(int height : H) {
+			while (!stack.isEmpty() && stack.peek() > height) {
+				stack.pop(); // Initialing block's size.
+			}
+			if (stack.isEmpty() || stack.peek() < height) {
+				stack.push(height); // New block's size.
+				count++;
+			}
+		}
+		return count;
+	}
+	
+}
