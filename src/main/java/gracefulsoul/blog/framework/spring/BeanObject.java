1749a9a81d081b4b3e52c4f7c04a6a5964d0dc27
yongsik <mymyyoo@naver.com>
2021-02-26 20:36:58 +0900

    Add posts about blog - Framework and Spring Framework.

---
+++ src/main/java/gracefulsoul/blog/framework/spring/BeanObject.java
@@ -0,0 +1,15 @@
+package gracefulsoul.blog.framework.spring;
+
+public class BeanObject {
+
+	private String name;
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+}
---
+++ src/main/java/gracefulsoul/blog/framework/spring/FieldInjection.java
@@ -0,0 +1,12 @@
+package gracefulsoul.blog.framework.spring;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Component;
+
+@Component
+public class FieldInjection {
+
+	@Autowired
+	private BeanObject beanObject;
+
+}
