fdf8d018141b694dc8fba9a49b70b682167b52a3
yongsik <mymyyoo@naver.com>
2021-02-28 13:34:14 +0900

    Add codility code - Lesson09. MaxDoubleSliceSum

--- src/main/java/gracefulsoul/codility/lesson09/MaxDoubleSliceSum.java
+++ src/main/java/gracefulsoul/codility/lesson09/MaxDoubleSliceSum.java
@@ -9,24 +9,24 @@ public class MaxDoubleSliceSum {
 	
 	// https://app.codility.com/demo/results/trainingZ9SZBR-5H4/
 	private static int solution(int[] A) {
-        int result = 0;
-        // Init subsum arrays.
-        int[] firstSubSum = new int[A.length];
-        for (int idx = 1; idx < A.length - 1; idx++) {
-            firstSubSum[idx] = Math.max(0, firstSubSum[idx - 1] + A[idx]);
-        }
-        int[] secondSubSum = new int[A.length];
-        for (int idx = A.length - 2; idx >= 1; idx--) {
-            secondSubSum[idx] = Math.max(0, secondSubSum[idx + 1] + A[idx]);
-        }
-        // Calculate max(result) value.
-        for (int idx = 1; idx < A.length - 1; idx++) {
-            int temp = firstSubSum[idx - 1] + secondSubSum[idx + 1];
-            if (temp > result) {
-                result = temp;
-            }
-        }
-        return result;
+		int result = 0;
+		// Init subsum arrays.
+		int[] firstSubSum = new int[A.length];
+		for (int idx = 1; idx < A.length - 1; idx++) {
+			firstSubSum[idx] = Math.max(0, firstSubSum[idx - 1] + A[idx]);
+		}
+		int[] secondSubSum = new int[A.length];
+		for (int idx = A.length - 2; idx >= 1; idx--) {
+			secondSubSum[idx] = Math.max(0, secondSubSum[idx + 1] + A[idx]);
+		}
+		// Calculate max(result) value.
+		for (int idx = 1; idx < A.length - 1; idx++) {
+			int temp = firstSubSum[idx - 1] + secondSubSum[idx + 1];
+			if (temp > result) {
+				result = temp;
+			}
+		}
+		return result;
 	}
 
 }
