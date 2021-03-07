715ae7f91a5cc423889a0a3bca933c4082df9e1c
yongsik <mymyyoo@naver.com>
2021-03-07 14:11:37 +0900

    Add codes about codility - Lesson12. ChocolatesByNumbers

--- src/main/java/gracefulsoul/codility/lesson12/ChocolatesByNumbers.java
+++ src/main/java/gracefulsoul/codility/lesson12/ChocolatesByNumbers.java
@@ -5,12 +5,12 @@ public class ChocolatesByNumbers {
 	public static void main(String[] args) {
 		System.out.println(solution(10, 4));
 	}
-
+	
 	// https://app.codility.com/demo/results/training2XBK6X-U2F/
 	private static int solution(int N, int M) {
 		return N / getGcd(M, N);
 	}
-
+	
 	// Euclidean algorithm.
 	private static int getGcd(int num1, int num2) {
 		if (num1 % num2 == 0) {
