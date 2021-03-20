df37411cada454e54b474783070352876e0f0365
yongsik <mymyyoo@naver.com>
2021-03-20 10:48:18 +0900

    Add codes about InterpreterPattern

--- src/main/java/gracefulsoul/designpattern/behavioral/mediator/ChatRoom.java
+++ src/main/java/gracefulsoul/designpattern/behavioral/mediator/ChatRoom.java
@@ -1,11 +1,11 @@
 package gracefulsoul.designpattern.behavioral.mediator;
 
-import java.util.Date;
+import java.time.LocalDateTime;
 
 public class ChatRoom {
 
 	public static void showMessage(User user, String message) {
-		System.out.println(new Date().toString() + " [" + user.getName() + "] : " + message);
+		System.out.println(LocalDateTime.now().toString() + " [" + user.getName() + "] : " + message);
 	}
 
 }
